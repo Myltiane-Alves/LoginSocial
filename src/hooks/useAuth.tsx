7ba4658dd72c244182a93a8a121582d12ef7ef18
Myltiane Alves <myltiane.alves@cleandev.com.br>
2022-04-18 19:22:27 -0300

    feat: iniciando login

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -40,13 +40,13 @@ function AuthProvider({ children }: AuthProviderData) {
 
   async function signIn() {
     try {
-      // set isLoggingIn to true
+      setIsLoggingIn(true)
 
-      // REDIRECT_URI - create OAuth redirect URI using makeRedirectUri() with "useProxy" option set to true
-      // RESPONSE_TYPE - set to "token"
-      // SCOPE - create a space-separated list of the following scopes: "openid", "user:read:email" and "user:read:follows"
-      // FORCE_VERIFY - set to true
-      // STATE - generate random 30-length string using generateRandom() with "size" set to 30
+      const REDIRECT_URI = makeRedirectUri({ useProxy: true});
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("openid: user:read:email user:read:follows");
+      const FORCE_VERIFY = true;
+      const STATE = generateRandom(30);
 
       // assemble authUrl with twitchEndpoint authorization, client_id, 
       // redirect_uri, response_type, scope, force_verify and state
